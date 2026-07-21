da0dea670b8b98ddfc74b5f752131618aee9b85c
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-21 14:09:16 +0900

    Quartz sync: Jul 21, 2026, 2:09 PM

--- content/index.md
+++ content/index.md
@@ -1,6 +1,7 @@
 ---
 title: lsgeun 개발 블로그 (🚧 만드는 중)
 tags:
+  - asdf
 created: 2026-07-17
 ---
 
--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -61,7 +61,11 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        
+        {/* ================= 검색 엔진 인증 태그 추가 ================= */}
+        <meta name="google-site-verification" content="lQyy4DrhpqO90ixWpWkoedAzW8mCSn8P-d0y9ir2hms" />
+        {/* ========================================================= */}
+        
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
